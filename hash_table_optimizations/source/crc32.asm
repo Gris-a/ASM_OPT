1e4eb8074a324f7c139c884022bce0e9ea2cde25
Завьялов Григорий Аркадьевич <z.gris.a.2005@gmail.com>
2024-04-08 09:57:26 +0300

    upd

--- hash_table_optimizations/source/crc32.asm
+++ hash_table_optimizations/source/crc32.asm
@@ -12,37 +12,112 @@ global CRC32
 ; ===============================================
 CRC32:
 ; -----------------------------------------------
-    dec rsi
+    push rdi
+    push rsi
+; -----------------------------------------------
     call strlen
 ; -----------------------------------------------
+    pop rsi
+    pop rdi
+; -----------------------------------------------
     mov rcx, rax
-    and rcx, 3
-    shr rax, 2
+    and rcx, 7
+    shr rax, 3
 ; -----------------------------------------------
-    xor edx, edx
-; -----------------------------------------------
-    test rcx, rcx
-    jz EndNext1
-Next1:
-    shl rdx, 8
-    mov dl, [rdi]
-    inc rdi
-    loop Next1
-EndNext1:
-; -----------------------------------------------
+    lea rdx, SwitchTable[rcx * 8]
     mov rcx, rax
     xor eax, eax
+    jmp [rdx]
+; -----------------------------------------------
+Remainder1:
+    mov dl, [rdi]
+    crc32 eax, dl
+
+    add rdi, 1
+    jmp Remainder0
+; -----------------------------------------------
+Remainder2:
+    mov dx, [rdi]
+    crc32 eax, dx
+
+    add rdi, 2
+    jmp Remainder0
+; -----------------------------------------------
+Remainder3:
+    mov dl, [rdi]
+    crc32 eax, dl
+
+    mov dx, [rdi + 1]
+    crc32 eax, dx
+
+    add rdi, 3
+    jmp Remainder0
+; -----------------------------------------------
+Remainder4:
+    mov edx, [rdi]
+    crc32 eax, edx
+
+    add rdi, 4
+    jmp Remainder0
+; -----------------------------------------------
+Remainder5:
+    mov dl, [rdi]
+    crc32 eax, dl
+
+    mov edx, [rdi + 1]
+    crc32 eax, edx
+
+    add rdi, 5
+    jmp Remainder0
+; -----------------------------------------------
+Remainder6:
+    mov dx, [rdi]
+    crc32 eax, dx
+
+    mov edx, [rdi + 2]
+    crc32 eax, edx
+
+    add rdi, 6
+    jmp Remainder0
+; -----------------------------------------------
+Remainder7:
+    mov dl, [rdi]
+    crc32 eax, dl
+
+    mov dx, [rdi + 1]
+    crc32 eax, dx
+
+    mov edx, [rdi + 3]
+    crc32 eax, edx
+
+    add rdi, 7
+; -----------------------------------------------
+Remainder0:
+    dec rsi
 ; -----------------------------------------------
     test rcx, rcx
-    jz EndNext2
-Next2:
-    crc32 eax, edx
+    jz EndNext
+Next:
     mov rdx, [rdi]
-    add rdi, 4
-    loop Next2
-EndNext2:
-    crc32 eax, edx
+    crc32 rax, rdx
+
+    add rdi, 8
+    loop Next
+EndNext:
 ; -----------------------------------------------
     and rax, rsi
     ret
 ; -----------------------------------------------
+
+section     .rodata
+align 8
+
+SwitchTable:
+    dq  Remainder0
+    dq  Remainder1
+    dq  Remainder2
+    dq  Remainder3
+    dq  Remainder4
+    dq  Remainder5
+    dq  Remainder6
+    dq  Remainder7
--- hash_table_optimizations/source/tester.c
+++ hash_table_optimizations/source/tester.c
@@ -1,25 +1,39 @@
+#include <assert.h>
 #include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "../include/hash_table.h"
 
+#define NWORDS 150000
+#define NCYCLES 6
+
+char **GetWords(void);
 inline int64_t TimeCounter(void) __attribute__((always_inline));
 
 int main(void)
 {
     struct HashTable *table = HashTableCtr(1 << 18);
+    char **words = GetWords();
 
-    char buf[MAX_KEY_LEN] = "";
-    while(scanf("%s", buf) != EOF)
+    int64_t start = TimeCounter();
+    for(size_t i = 0; i < NCYCLES; i++)
     {
-        volatile bool aboba = HashTableInsert(table, buf, 0);
+        for(size_t j = 0; j < NWORDS; j++)
+        {
+            volatile bool aboba = HashTableInsert(table, words[j], 0);
+        }
     }
+    int64_t end = TimeCounter();
 
-    for(size_t i = 0; i < table->size_max; i++)
+    fprintf(stderr, "\n\n<\ttime neasured: %ld>\n\n", end - start);
+
+    for(size_t i = 0; i < NWORDS; i++)
     {
-        printf("%zu\t%zu\n", i, table->data[i].size);
+        free(words[i]);
     }
-
+    free(words);
     table = HashTableDtr(table);
 }
 
@@ -33,11 +47,28 @@ inline int64_t TimeCounter(void)
         "rdtsc\n\t"
         "shl rdx, 32\n\t"
         "add rax, rdx\n\t"
+        "mov %0, rax\n\t"
         ".att_syntax prefix\n\t"
-        : "=a"(result)
+        : "=r"(result)
         :
         : "%rdx", "%rax"
     );
 
     return result;
+}
+
+char **GetWords(void)
+{
+    char **words = (char **)calloc(NWORDS, sizeof(char *));
+    assert(words);
+
+    char buf[MAX_KEY_LEN] = "";
+    for(size_t i = 0; i < NWORDS; i++)
+    {
+        scanf("%s", buf);
+        words[i] = strdup(buf);
+        assert(words[i]);
+    }
+
+    return words;
 }
